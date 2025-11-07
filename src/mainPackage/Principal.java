7df247cea13815f081dd9330b2877b28bba19b0c
Benjamin Caceres <benjamin.caceres.ra@gmail.com>
2025-11-06 21:45:50 -0300

    feat: working in Usuario class

--- src/mainPackage/Principal.java
+++ src/mainPackage/Principal.java
@@ -1,7 +1,16 @@
 package mainPackage;
 
+import java.util.Arrays;
+
 public class Principal {
 	public static void main(String args[]) {
-		System.out.println("Hola mundo!");
+		Usuario user = Usuario.crear(
+			"Benjamin Caceres", 
+			"19617271-8", 
+			"M", 
+			"Ing. Informatica",
+			"Prestamo"
+		);
+		System.out.println(user);
 	}
 }
--- src/mainPackage/Usuario.java
+++ src/mainPackage/Usuario.java
@@ -1,5 +1,47 @@
 package mainPackage;
 
 public class Usuario {
-
+	String nombreCompleto;
+	String rut;
+	String genero;
+	String carrera;
+	String prestamo;
+	
+	protected Usuario(String nombreCompleto, String rut, String genero, String carrera, String prestamo) {
+		this.nombreCompleto = nombreCompleto;
+		this.genero = genero;
+		this.carrera = carrera;
+		this.prestamo = prestamo;
+		this.rut = rut;
+	}
+	
+	public static Usuario crear(String nombreCompleto, String rut, String genero, String carrera, String prestamo) {
+		return new Usuario(nombreCompleto, rut, genero, carrera, prestamo);
+	}
+	
+	/* Función que devuelve true si el rut pasado como argumento es válido y false si no lo es */
+	protected boolean isValidRut(String rut) {
+		rut = rut.replace(".", "");
+		rut = rut.toUpperCase();
+		
+		/* Crea un array que contiene en el índice 0 la parte numérica del rut y en el índice 1 el dígito verificador */
+		String[] parts = rut.split("-");
+		
+		/* Crea un array que contiene sólo el primer item del array 'parts' */
+		String numericPart = parts[0];
+		
+		
+		return true;
+	}
+	
+	@Override
+    public String toString() {
+        return "Usuario(" +
+                "nombre: '" + nombreCompleto + '\'' +
+                ", rut: '" + rut + '\'' +
+                ", genero: '" + genero + '\'' +
+                ", carrera: '" + carrera + '\'' +
+                ", prestamo: '" + prestamo + '\'' +
+                ')';
+    }
 }
